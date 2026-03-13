5af368d566c458e5c402bb5e2af62bdf279e9d08
siddhartha2083 <45175824+siddhartha2083@users.noreply.github.com>
2026-03-13 11:02:53 +0000

    made modifications in devcontainer

---
+++ code-01/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountName = "Siddhartha"
+var accountEmail = "siddhartha2083@gmail.com"
+accountCity = "Jaipur"
+let accountState
+
+console.table([accountId, accountName, accountEmail, accountCity, accountState])
